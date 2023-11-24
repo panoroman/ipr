b1896ae716ef068e30127c95f101afca9427c12b
Roman Panfilov <roman.panfilov@softservice.org>
2023-11-24 15:15:03 +0700

    init

---
+++ src/test/java/com/example/ipr/IprApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ipr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IprApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
